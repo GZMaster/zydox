94126bc8776d1fd5a886a5c8ac7d4f4a2fc39876
GZMaster <danigamester@gmail.com>
2022-10-05 14:19:17 +0100

    resolve

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,42 @@
 import React from "react";
 import "./Footer.scss";
 import { FaFacebookF, FaInstagram, FaTwitter } from "react-icons/fa";
+
 const Footer = () => {
   return (
     <footer className="Footer">
-      
+      <div className="F_text">
+        <p>Copyright © 2022 zydoxoilandgasservices. All rights reserved.</p>
+        <p>
+          Designed by <span className="F_Team">Retro Developers</span>
+        </p>
+      </div>
+      <div className="F_Socials">
+        <a
+          href="https://www.facebook.com/profile.php?id=100084729275588"
+          className="F_item"
+          target={"_blank"}
+          rel="noreferrer"
+        >
+          <FaFacebookF />
+        </a>
+        <a
+          href="https://www.linkedin.com/company/bralewood-investment-limited/"
+          className="F_item"
+          target={"_blank"}
+          rel="noreferrer"
+        >
+          <FaLinkedin />
+        </a>
+        <a
+          href="https://www.instagram.com/bralewoodinvestments/"
+          className="F_item"
+          target={"_blank"}
+          rel="noreferrer"
+        >
+          <FaInstagram />
+        </a>
+      </div>
     </footer>
   );
 };
